bd50bf2ac26761fd123a736bb9122ebf56fbb0d3
Miranda Guimarães <armg@gft.com>
2022-09-14 12:15:13 -0300

    [Bug Fix] Changing to HashRouter.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import App from "./App";
 import { FavoritesContextProvider } from "./store/favorites-context";
 import "./index.css";
@@ -8,8 +8,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <FavoritesContextProvider>
-    <BrowserRouter>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter >
   </FavoritesContextProvider>
 );
